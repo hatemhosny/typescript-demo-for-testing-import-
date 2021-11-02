29b328154267b36d77bfac284bc285784757c473
Hatem Hosny <hatemhosny@users.noreply.github.com>
2021-11-02 02:14:25 +0200

    initial commit!

---
+++ src/script.ts
@@ -0,0 +1,27 @@
+class Counter {
+  private count: number;
+  constructor() {
+    this.count = 0;
+  }
+  increment() {
+    this.count += 1;
+  }
+  getValue() {
+    return this.count;
+  }
+}
+
+const title = document.querySelector("#title");
+const count = document.querySelector("#counter");
+const button = document.querySelector("#counter-button");
+
+title.textContent = "TypeScript";
+const counter = new Counter();
+button.addEventListener(
+  "click",
+  () => {
+    counter.increment();
+    count.textContent = String(counter.getValue());
+  },
+  false
+);
